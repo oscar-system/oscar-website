13eab9d8cdf7159646476d87078333fd56e457cd
Aaruni Kaushik <aaruni@edufor.me>
2024-11-27 16:08:50 +0100

    Update contributors on 2024-11-27

--- etc/update-contributors.py
+++ etc/update-contributors.py
@@ -33,7 +33,7 @@ github_userlist = []
 API_KEY = os.getenv("API_KEY") # TODO: rename to whatever is the right env var
 
 # grab currently active devs
-if not os.path.isdir("repos"): #bla
+if not os.path.isdir("repos"): #bla a
     os.mkdir("repos")
 os.chdir("repos")
 for repo in repoList:
