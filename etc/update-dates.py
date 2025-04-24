f0d94d3de2e4b25ba269e23a4b8a38c6371cbd68
Max Horn <max@quendi.de>
2025-04-24 14:23:39 +0200

    Revert "Rewrite in python / add fallback for network failures"
    This reverts commits cecdffff3edbf9bc9481236430a1cb548bc66fc2
    and 4d78ee8c7f176270efef951443d099d233fc9b42.
    
    Unfortunately the Python code does not run on the server.

--- etc/update-dates.py
+++ etc/update-dates.py
@@ -1,46 +1,37 @@
-#!/usr/bin/env python3
-
-import os
+import json
 import yaml
+import requests
 
 from datetime import datetime
-from github import Github, Auth
 
-API_KEY = os.getenv("API_KEY")
-
-auth = Auth.Token(API_KEY)
-
-g = Github(auth=auth)
-failed=False
-
-if os.getcwd().split('/')[-1] == 'etc':
-    ogfile = yaml.safe_load(open("../_data/examples.yml"))
-else:
-    ogfile = yaml.safe_load(open("_data/examples.yml"))
+ogfile = yaml.safe_load(open("../_data/examples.yml"))
 
 for i in range(len(ogfile)):
+    #assemble URL
+    repo = f"{ogfile[i]['repository']}"
+    filepath = f"{ogfile[i]['filename']}.ipynb"
+    url = f'https://api.github.com/repos/{repo}/commits?path={filepath}'
+
+    # fallback default date
+    dt = datetime(1970, 1, 1)
 
     try:
-        # all network requests are in this try block
-        reponame = f"{ogfile[i]['repository']}"
-        filepath = f"{ogfile[i]['filename']}.ipynb"
-        print(f"Getting {reponame}/{filepath}...")
-        repo = g.get_repo(reponame)
-        commit = repo.get_commits(path=filepath)[0]
-        dt = commit.stats.last_modified_datetime
+        #make request
+        #unauth API is rate limited at 60 per hour
+        #this should be okay for running script once per hour (we have less than 60 tutorials)
+        #auth API usage has limit of 5,000 requests per hour.
+        r = requests.get(url)
+        if r.status_code != 200:
+            raise ValueError(f"Reponse must have code 200, we got {r.status_code}")
+        r = json.loads(r.content)
+        dt = datetime.strptime(r[0]['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")    
     except Exception as e:
         print(e)
-        print("Network access failed. Using fallback default date.")
-        dt = datetime(1970, 1, 1)
+        print(f"Got an error {e}.\nUsing default date for {ogfile[i]['filename']}...")
 
     #update times
     d = dt.date().strftime("%B %d, %Y")
     ogfile[i]['date'] = d
 
-if os.getcwd().split('/')[-1] == 'etc':
-    outfilepath = '../_data/examples_with_updated_last_modified.yml'
-else:
-    outfilepath = "_data/examples_with_updated_last_modified.yml"
-
-with open(outfilepath, 'w') as outfile:
+with open('../_data/examples_with_updated_last_modified.yml', 'w') as outfile:
     outfile.write(yaml.dump(ogfile))
--- etc/update.sh
+++ etc/update.sh
@@ -15,17 +15,24 @@ bundle config set --local path 'vendor/bundle'
 bundle install
 
 # get tutorial status
-echo "Getting tutorial status....."
-./etc/tutorial_status.py
-echo "Done!"
+statusurl=$(curl -SsL --retry 5 "https://api.github.com/repos/oscar-system/TutorialTesterforOscar/actions/workflows/CI.yml/runs?per_page=1" | jq  '.workflow_runs[0].jobs_url' | sed 's/"//g')
+curl -SsL --retry 5 $statusurl | jq '.["jobs"][1:] | .[] | .name+":"+.conclusion' | sed 's/^.*\s.*\s.*\s//' | sed 's/):/: /' | sed s'/"$//' > _data/examples_status.yml
 # get tutorial last modified dates
-echo "Getting tutorial last modified dates......."
-./etc/update-dates.py
-echo "Done!"
+cd etc
+python3 update-dates.py
+cd ..
 # update version info
-echo "Getting version info......."
-./etc/update-latest-release.py
-echo "Done!"
+curl -SsL https://api.github.com/repos/oscar-system/Oscar.jl/releases/latest > latest.json
+version=$(jq '.["name"]' latest.json | sed 's/v//')
+date=$(jq '.["created_at"]' latest.json | cut -c2-11)
+year=$(echo $date | tr '-' ' ' | awk '{print $1}')
+month=$(echo $date | tr '-' ' ' | awk '{print $2}')
+day=$(echo $date | tr '-' ' ' | awk '{print $3}')
+echo "version: $version" > _data/release.yml
+echo "year: \"$year\"" >> _data/release.yml
+echo "month: \"$month\"" >> _data/release.yml
+echo "day: \"$day\"" >> _data/release.yml
+echo "date: \"$year-$month-$day\"" >> _data/release.yml
+rm latest.json
 # run jekyll
-echo "Running jekyl......."
-bundle exec jekyll build --config _config.yml,_config_production.yml # -d /srv/www/www-mathe-oscar/data/http
+bundle exec jekyll build --config _config.yml,_config_production.yml -d /srv/www/www-mathe-oscar/data/http
