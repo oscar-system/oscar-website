a4207a6892d739e674d5ae74cd1b38e99cfc1901
Aaruni Kaushik <aaruni@edufor.me>
2025-05-20 07:46:23 +0200

    Reapply "Rewrite in python / add fallback for network failures"
    This reverts commit f0d94d3de2e4b25ba269e23a4b8a38c6371cbd68.

--- etc/update-dates.py
+++ etc/update-dates.py
@@ -1,37 +1,46 @@
-import json
+#!/usr/bin/env python3
+
+import os
 import yaml
-import requests
 
 from datetime import datetime
+from github import Github, Auth
 
-ogfile = yaml.safe_load(open("../_data/examples.yml"))
+API_KEY = os.getenv("API_KEY")
+
+auth = Auth.Token(API_KEY)
+
+g = Github(auth=auth)
+failed=False
+
+if os.getcwd().split('/')[-1] == 'etc':
+    ogfile = yaml.safe_load(open("../_data/examples.yml"))
+else:
+    ogfile = yaml.safe_load(open("_data/examples.yml"))
 
 for i in range(len(ogfile)):
-    #assemble URL
-    repo = f"{ogfile[i]['repository']}"
-    filepath = f"{ogfile[i]['filename']}.ipynb"
-    url = f'https://api.github.com/repos/{repo}/commits?path={filepath}'
-
-    # fallback default date
-    dt = datetime(1970, 1, 1)
 
     try:
-        #make request
-        #unauth API is rate limited at 60 per hour
-        #this should be okay for running script once per hour (we have less than 60 tutorials)
-        #auth API usage has limit of 5,000 requests per hour.
-        r = requests.get(url)
-        if r.status_code != 200:
-            raise ValueError(f"Response must have code 200, we got {r.status_code}")
-        r = json.loads(r.content)
-        dt = datetime.strptime(r[0]['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")    
+        # all network requests are in this try block
+        reponame = f"{ogfile[i]['repository']}"
+        filepath = f"{ogfile[i]['filename']}.ipynb"
+        print(f"Getting {reponame}/{filepath}...")
+        repo = g.get_repo(reponame)
+        commit = repo.get_commits(path=filepath)[0]
+        dt = commit.stats.last_modified_datetime
     except Exception as e:
         print(e)
-        print(f"Got an error {e}.\nUsing default date for {ogfile[i]['filename']}...")
+        print("Network access failed. Using fallback default date.")
+        dt = datetime(1970, 1, 1)
 
     #update times
     d = dt.date().strftime("%B %d, %Y")
     ogfile[i]['date'] = d
 
-with open('../_data/examples_with_updated_last_modified.yml', 'w') as outfile:
+if os.getcwd().split('/')[-1] == 'etc':
+    outfilepath = '../_data/examples_with_updated_last_modified.yml'
+else:
+    outfilepath = "_data/examples_with_updated_last_modified.yml"
+
+with open(outfilepath, 'w') as outfile:
     outfile.write(yaml.dump(ogfile))
