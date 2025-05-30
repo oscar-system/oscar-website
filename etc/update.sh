4b151e572c08924776f0c247ede6a780e1b4ef9e
Max Horn <max@quendi.de>
2025-05-30 12:11:36 +0200

    Fix etc/update.sh

--- etc/update.sh
+++ etc/update.sh
@@ -18,7 +18,7 @@ bundle install
 if [ ! -d ".venv" ]; then
     python3 -m venv .venv
 fi
-source .venv/bin/activate
+. .venv/bin/activate
 
 # install requirements for the python scripts
 python3 -m pip install -r etc/requirements.txt
