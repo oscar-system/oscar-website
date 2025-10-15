9deb806b22a63088fa9ecd32ada28c733de82e57
Aaruni Kaushik <aaruni96@users.noreply.github.com>
2025-10-15 15:02:21 +0200

    Add repo to people_list

--- etc/update-contributors.py
+++ etc/update-contributors.py
@@ -8,7 +8,8 @@ import subprocess
 def custom_sort_function(item):
     name, _ = item
     sortweight = {"name": 0, "affiliation": 1, "email": 2, "github": 3, "website": 4,
-                  "paid_by_dfg": 5,"status": 6, "comment": 7, "aka": 8, "aka_email": 9}
+                  "paid_by_dfg": 5,"status": 6, "comment": 7, "aka": 8, "aka_email": 9,
+                  "repos": 10}
     return sortweight[name]
 
 # Hack copied from https://github.com/yaml/pyyaml/issues/127#issuecomment-525800484
@@ -24,6 +25,11 @@ class MyDumper(yaml.SafeDumper):
 infile = "../_data/people_list.yml"
 with open(infile, "r") as ymlfile:
     peopleList = yaml.safe_load(ymlfile)
+
+for i in peopleList:
+    if 'repos' not in i.keys():
+        i['repos'] = []
+
 names = [i['github'] for i in peopleList]
 repoList = ["thofma/Hecke.jl", "oscar-system/Oscar.jl", "Nemocas/Nemo.jl",
             "Nemocas/AbstractAlgebra.jl", "oscar-system/GAP.jl", "oscar-system/Polymake.jl",
@@ -133,11 +139,19 @@ for repo in repoList:
             newpersonlist.append(i[0])
             print(f"{i[0]}\t{i[1]}\t{github_username}")
             github_newusers.append(github_username)
-            newList.append([i[0], i[1], github_username])
-        github_userlist.append(github_username)
+            newList.append([i[0], i[1], github_username, [repo]])
+        elif github_username in names:
+            user = [item for item in peopleList if item['github'] == github_username][0]
+            if repo not in user['repos']:
+                user['repos'].append(repo)
+        else:
+            # github_username in github_newusers
+            user = [item for item in newList if item[2] == github_username][0]
+            if repo not in user[3]:
+                user.append(repo)
+        if github_username not in github_userlist:
+            github_userlist.append(github_username)
     os.chdir("..")
-
-github_userlist = list(set(github_userlist))
 
 # mark active / retired
 # if PI, don't touch them
@@ -164,10 +178,10 @@ for i in peopleList:
 np = []
 for i in newList:
     if "users.noreply.github.com" in i[1]:
-        np.append({"name": i[0], "github": i[2], "status": "active"})
+        np.append({"name": i[0], "github": i[2], "status": "active", "repos": i[3]})
         summarystring += f"- Email not found for {i[0]} ({i[2]})..!\n"
     else:
-        np.append({"name": i[0], "email": i[1], "github": i[2], "status": "active"})
+        np.append({"name": i[0], "email": i[1], "github": i[2], "status": "active", "repos": i[3]})
 peopleList.extend(np)
 
 sortedPeopleList = sorted(peopleList, key= lambda d: d['name'].split()[-1])
