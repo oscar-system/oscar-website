d2477a81f64112c10d92b6a5bde3b0c191bad69a
Martin Bies <MBies87@googlemail.com>
2025-10-31 11:02:15 +0100

    Add CI job for last modified dates of tutorials

--- etc/update-dates.py
+++ etc/update-dates.py
@@ -7,7 +7,7 @@ import yaml
 from datetime import datetime
 from github import Github, Auth
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = (os.getenv("API_KEY") or os.getenv("GITHUB_TOKEN") or "").strip()
 if API_KEY == None:
     print("API key was not found! Authentication will fail!\nSet the environment variable API_KEY "
           "to a github access token and try again!\n")
