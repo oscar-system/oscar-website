6001777592ef30949d74e41e6e4249afec475507
Martin Bies <MBies87@googlemail.com>
2025-11-01 14:39:42 +0100

    Remove unused lines

--- etc/update-contributors.py
+++ etc/update-contributors.py
@@ -192,12 +192,6 @@ for repo in REPO_LIST:
         count = count+1
         print(f"Item {count} of {len(dnamelist)}...")
         print(i)
-        if i[0] == 'dependabot[bot]':
-            print("Skipping dependabot!")
-            continue
-        if "[bot]" in i[0]:
-            print(f"Skipping suspected bot {i[0]}")
-            continue
         email = i[1]
         process = subprocess.run(['git', 'log', f'--author={email}', '--format=%H', '-n 1'],
                                    capture_output=True)
