a0cdc2997966bb94ec718cb54626a006715f56ec
Aaruni Kaushik <aaruni@edufor.me>
2025-12-08 15:49:16 +0100

    linting: use upper case for constants

--- etc/update-latest-release.py
+++ etc/update-latest-release.py
@@ -10,12 +10,11 @@ import yaml
 
 # 2. Constants
 TARGET_REPO = os.getenv("TARGET_REPO", "oscar-system/Oscar.jl")
-JULIA_MIN = os.getenv("JULIA_MIN", "1.10")
-ownpath = os.path.abspath(sys.argv[0])
-repopath = os.path.dirname(os.path.dirname(ownpath))
-datapath = os.path.join(repopath, "_data")
-releasefilepath = os.path.join(datapath, "release.yml")
-
+JULIA_MIN_DEFAULT = "1.10"
+OWNPATH = os.path.abspath(sys.argv[0])
+REPOPATH = os.path.dirname(os.path.dirname(OWNPATH))
+DATAPATH = os.path.join(REPOPATH, "_data")
+RELEASEFILEPATH = os.path.join(DATAPATH, "release.yml")
 
 # 3. Function to write simple key=value outputs for GitHub Actions.
 def gh_output(**kvs):
@@ -69,19 +68,18 @@ dt = dt.replace(tzinfo=timezone.utc)
 
 # 8. Read old version
 old_version = None
-if os.path.exists(releasefilepath):
-    with open(releasefilepath, "r") as yamlfile:
+if os.path.exists(RELEASEFILEPATH):
+    with open(RELEASEFILEPATH, "r", encoding="utf-8") as yamlfile:
         data = yaml.safe_load(yamlfile)
         old_version = data['version']
 else:
-    print(f"Release file not found: {releasefilepath}")
-    gh_output(changed="false", reason="release-file-missing")
+    print(f"Release file not found: {RELEASEFILEPATH}")
     sys.exit(1)
 
 
 # 9. Early exist in case old version and new version agree
 if old_version == version:
-    print(f"{releasefilepath} already has latest version {version}. Nothing to do.")
+    print(f"{RELEASEFILEPATH} already has latest version {version}. Nothing to do.")
     gh_output(
         changed="false",
         reason="up-to-date",
@@ -92,18 +90,22 @@ if old_version == version:
     sys.exit(0)
 
 
-# 10. Otherwise, write new release information and signal changes to github workflow
-releasestring = f"""version: "{version}"
+# Write new release information and signal changes to github workflow
+RELEASESTRING = f"""version: "{version}"
 year: "{dt.year}"
 month: "{dt.month}"
 day: "{dt.day}"
 date: "{dt.date()}"
 julia-min: "{JULIA_MIN}"
 """
-print(f"releasefilepath is {releasefilepath}")
-with open(releasefilepath, "w", encoding="utf-8") as releasefile:
-    releasefile.write(releasestring)
-print(f"Updated {releasefilepath} to version {version} (was {old_version or 'none'}).")
+
+print(f"RELEASEFILEPATH is {RELEASEFILEPATH}")
+
+with open(RELEASEFILEPATH, "w", encoding="utf-8") as releasefile:
+    releasefile.write(RELEASESTRING)
+
+print(f"Updated {RELEASEFILEPATH} to version {version} (was {old_version or 'none'}).")
+
 gh_output(
     changed="true",
     reason="updated",
